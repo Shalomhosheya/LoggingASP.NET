a531a6122cb037674feb0ce9993d742c76b1f754
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-11-08 23:22:37 +0530

    logging error exception handled

--- WebLoggingDotNet/Program.cs
+++ WebLoggingDotNet/Program.cs
@@ -12,6 +12,15 @@ app.MapGet("/", (ILogger<Program> logger) =>
     logger.LogInformation("Hello endpoint was called at {time}", DateTime.Now);
     logger.LogWarning("don't log in");
     logger.LogError(new Exception(), "don't log error");
+    try
+    {
+       throw new Exception("logging failed");
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex,"Application Error.....Time: {time}: ",DateTime.Now);
+    }
+    
     return "Hello World!";
 });
 
